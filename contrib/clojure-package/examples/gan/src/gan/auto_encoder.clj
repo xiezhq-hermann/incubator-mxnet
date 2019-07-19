a6119aed919c1984df942377633f3a213c945e47
gigasquid <cmeier@gigasquidsoftware.com>
2019-07-19 19:26:51 -0400

    input with label

--- contrib/clojure-package/examples/gan/src/gan/auto_encoder.clj
+++ contrib/clojure-package/examples/gan/src/gan/auto_encoder.clj
@@ -25,34 +25,35 @@
 
 ;;; Load the MNIST datasets
 ;;; note that the label is the same as the image
-(def train-data (mx-io/mnist-iter {:image (str data-dir "train-images-idx3-ubyte")
-                                       :label (str data-dir "train-images-idx3-ubyte")
-                                        ;:input-shape [1 28 28]
-                                        :input-shape [784]
-                                        :label-shape [10]
-                                        :flat true
-                                        :batch-size batch-size
-                                        :shuffle true}))
-
-(def test-data (mx-io/mnist-iter {:image (str data-dir "train-images-idx3-ubyte")
-                                       :label (str data-dir "train-images-idx3-ubyte")
-                                       ;;:input-shape [1 28 28]
-                                  :input-shape [784]
+(defonce train-data (mx-io/mnist-iter {:image (str data-dir "train-images-idx3-ubyte")
+                                       :label (str data-dir "train-labels-idx1-ubyte")
+                                       :label-name "softmax_label"
+                                       :input-shape [784]
                                        :batch-size batch-size
+                                       :shuffle true
                                        :flat true
-                                       :shuffle true}))
+                                       :silent false
+                                       :seed 10}))
+
+(defonce test-data (mx-io/mnist-iter {:image (str data-dir "t10k-images-idx3-ubyte")
+                                      :label (str data-dir "t10k-labels-idx1-ubyte")
+                                      :input-shape [784]
+                                      :batch-size batch-size
+                                      :flat true
+                                      :silent false}))
+
 (def output (sym/variable "input_"))
+
+(comment
+  (def x (mx-io/next test-data))
+
+  (mx-io/batch-label x)
+
+
+  )
 
 (defn get-symbol []
   (as-> (sym/variable "input") data
-    ;; encode
-    (sym/fully-connected "encode1" {:data data :num-hidden 100})
-    (sym/activation "sigmoid1" {:data data :act-type "sigmoid"})
-
-    ;; encode
-    (sym/fully-connected "encode2" {:data data :num-hidden 50})
-    (sym/activation "sigmoid2" {:data data :act-type "sigmoid"})
-
     ;; decode
     (sym/fully-connected "decode1" {:data data :num-hidden 50})
     (sym/activation "sigmoid3" {:data data :act-type "sigmoid"})
@@ -70,9 +71,10 @@
     ))
 
 (def data-desc (first (mx-io/provide-data-desc train-data)))
+(def label-desc (first (mx-io/provide-label-desc train-data)))
 
 (def model (-> (m/module (get-symbol) {:data-names ["input"] :label-names ["input_"]})
-               (m/bind {:data-shapes [(assoc data-desc :name "input")]
+               (m/bind {:data-shapes [(assoc label-desc :name "input")]
                         :label-shapes [(assoc data-desc :name "input_")]})
                (m/init-params {:initializer  (initializer/uniform 1)})
                (m/init-optimizer {:optimizer (optimizer/adam {:learning-rage 0.001})})))
@@ -86,7 +88,7 @@
      train-data
      (fn [batch]
        (-> model
-           (m/forward {:data (mx-io/batch-data batch) :label (mx-io/batch-data batch)})
+           (m/forward {:data (mx-io/batch-label batch) :label (mx-io/batch-data batch)})
            (m/update-metric my-metric (mx-io/batch-data batch))
            (m/backward)
            (m/update))))
@@ -98,26 +100,43 @@
   (mx-io/provide-label train-data)
   (mx-io/reset train-data)
   (def my-batch (mx-io/next train-data))
+  (def train-labels (mx-io/batch-label my-batch))
   (def images (mx-io/batch-data my-batch))
   (ndarray/shape (ndarray/reshape (first images) [100 1 28 28]))
   (viz/im-sav {:title "originals" :output-path "results/" :x (ndarray/reshape (first images) [100 1 28 28])})
 
 
+  (ndarray/shape (first train-labels))
   (train 3)
 
 
   (def my-test-batch (mx-io/next test-data))
   (def test-images (mx-io/batch-data my-test-batch))
-  (def preds (m/predict-batch model {:data test-images} ))
+  (def test-labels (mx-io/batch-label my-test-batch))
+  (def preds (m/predict-batch model {:data test-labels} ))
   (viz/im-sav {:title "preds" :output-path "results/" :x (ndarray/reshape (first preds) [100 1 28 28])})
-  
+
+  (def x (ndarray/array (into [] (repeat 100 2)) [100]))
+  (def new-preds (m/predict-batch model {:data [x]} ))
+  (viz/im-sav {:title "preds" :output-path "results/" :x (ndarray/reshape (first new-preds) [100 1 28 28])})
+
+
+  (ndarray/slice (first test-labels) 0 1) 
+  (ndarray/slice (first preds) 0 1)
+
+  (first images)
+  (ndarray/div (first train-labels) 255.0)
+  (ndarray/as-type (first train-labels) dtype/UINT8)
+  (-> (ndarray/to-scalar (ndarray/slice (first test-labels) 1 2))
+      (/ 255.0))
+
+    (-> (ndarray/to-scalar (ndarray/slice (first train-labels) 0 1)))
+
+  (/ 231.0 255)
 
 
   (sym/list-arguments (m/symbol my-mod))
   (def data-desc (first (mx-io/provide-data-desc train-data)))
-
-
-
 
 
 
